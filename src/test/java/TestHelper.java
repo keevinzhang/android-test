edd1a3990cdff940e72ed7d05f7361f54781fd5c
Kevin Zhang <kevin.zhang@bokhandelsgruppen.se>
2022-01-17 11:19:55 +0100

    [ci skip] Updated apk version to 2.4.10-2201101518

--- src/test/java/TestHelper.java
+++ src/test/java/TestHelper.java
@@ -54,7 +54,7 @@ public class TestHelper {
         capabilities.setCapability("platformName", "android");
         capabilities.setCapability("platformVersion", "11.0");
         capabilities.setCapability("deviceName", "Samsung Galaxy S21");
-        capabilities.setCapability("app", "bs://cd381025fb511d9cc7ad32f60b27d48fbb243d80");
+        capabilities.setCapability("app", "bs://ab0a730c6b71ae459ce1e04d39a73194efd7cc62");
 
         //capabilities.setCapability("deviceName", "10.0.0.7:5555"); //OnePlus 6T
         //capabilities.setCapability("deviceName", "10.0.0.23:5555"); //Galaxy S10
